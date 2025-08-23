ca7a079af4c4be07198d67a7e307aa068df75fc0
kunal-prakash <kp250796@gmail.com>
2025-08-23 16:24:11 +0530

    basic crud operations

--- src/app.js
+++ src/app.js
@@ -3,19 +3,90 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json());
+
+//Signup api - POST /signup -  create a new user
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "John",
-    lastName: "Doe",
-    emailId: "john@doe.com",
-    password: "pass13",
-  });
+  const userData = req.body;
+  const user = new User(userData);
   try {
     await user.save();
     res.send("User created successfully");
   } catch (err) {
     res.status(400).end("Error creating user: ", err.message);
   }
+});
+
+//Get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    const user = await User.findOne({ emailId: userEmail });
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Feed api -  GET /feed - get all the users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No users found");
+    }
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Delete user from the DB - DELETE /user - deletes a user by ID
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Update user in DB - PATCH /user - can update partial user data(document) by ID
+app.patch("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    // await User.findOneAndUpdate({_id: userId}, req.body);
+    const user = await User.findByIdAndUpdate(userId, req.body, {
+      returnDocument: "after",
+    });
+    //defult returnDocument: "before" which returns the document before update
+    console.log(user);
+    res.send("User data updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Update user in DB - PATCH /user - replaces the entire user data(document) by ID
+app.put("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    await User.findOneAndReplace({ _id: userId }, req.body);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//WildCard Error handling
+app.use((err, req, res, next) => {
+  console.error(`Error occurred at ${req.path}: `, err.message);
+  res.status(500).send("Something went wrong");
 });
 
 connectDB()
